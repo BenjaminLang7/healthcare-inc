08e154cb66ae3ada2349ee23331da60cbacabd9a
Dexter Sung <dextersung18@gmail.com>
2022-03-30 15:41:19 -0500

    fixed up models and controllers and templates

--- flask_app/models/date.py
+++ flask_app/models/date.py
@@ -1,6 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import app
-
 from flask_app.models import user
 
 from flask import flash
@@ -16,14 +15,129 @@ class Date:
         self.updated_at = data['updated_at']
         self.creator = None
 
-## CREATE ##
     @classmethod
-    def register(cls, data ):
+    def create_one(cls, data ):
         query = """
-        INSERT INTO users ( first_name, last_name, email, password, insurance_providers_id, created_at, updated_at ) 
-        VALUES ( %(first_name)s , %(last_name)s , %(email)s, %(password)s, %(insurance_providers_id)s, NOW() , NOW() )
+        INSERT INTO dates ( care_date, provider_name, created_at, updated_at, users_id) 
+        VALUES ( %(care_date)s , %(provider_name)s, NOW() , NOW(), %(users_id)s )
         ;"""
         results = connectToMySQL(cls.db).query_db(query, data)
         flash("Account creation successful")
         print(results)
         return results
+
+    @classmethod
+    def validate_create(cls, data):
+        is_valid = True
+        if not data:
+            flash("Please enter a date")
+            is_valid = False
+        return is_valid
+
+    @classmethod
+    def get_all_with_creator(cls):
+        query = """
+        SELECT * FROM dates 
+        LEFT JOIN users ON dates.users_id = users.id
+        ;"""
+        results = connectToMySQL(cls.db).query_db(query)
+        print(results)
+        all_dates = []
+        for one_date in results:
+            date_data = {
+                "id": one_date['id'],
+                "care_date": one_date['care_date'],
+                "provider_name": one_date['provider_name'],
+                "created_at": one_date['created_at'],
+                "updated_at": one_date['updated_at']
+            }
+            single_date = cls(date_data)
+            user_data = {
+                "id": one_date['users.id'],
+                "first_name": one_date['first_name'],
+                "last_name": one_date['last_name'],
+                "email": one_date['email'],
+                "insurance_name": one_date['insurance_name'],
+                "password": one_date['password'],
+                "created_at": one_date['users.created_at'],
+                "updated_at": one_date['users.updated_at']
+            }
+            single_user = user.User(user_data)
+            single_date.creator = single_user
+            all_dates.append(single_date)
+        return all_dates
+
+    @classmethod
+    def destroy(cls, data):
+        query = "DELETE FROM dates WHERE dates.id=%(id)s;"
+        results = connectToMySQL(cls.db).query_db(query,data)
+        print(results)
+        return results
+
+    @classmethod
+    def sort_by_date(cls):
+        query = """
+        SELECT * FROM dates 
+        LEFT JOIN users ON dates.users_id = users.id
+        ORDER BY care_date ASC
+        ;"""
+        results = connectToMySQL(cls.db).query_db(query)
+        print(results)
+        all_dates = []
+        for one_date in results:
+            date_data = {
+                "id": one_date['id'],
+                "care_date": one_date['care_date'],
+                "provider_name": one_date['provider_name'],
+                "created_at": one_date['created_at'],
+                "updated_at": one_date['updated_at']
+            }
+            single_date = cls(date_data)
+            user_data = {
+                "id": one_date['users.id'],
+                "first_name": one_date['first_name'],
+                "last_name": one_date['last_name'],
+                "email": one_date['email'],
+                "insurance_name": one_date['insurance_name'],
+                "password": one_date['password'],
+                "created_at": one_date['users.created_at'],
+                "updated_at": one_date['users.updated_at']
+            }
+            single_user = user.User(user_data)
+            single_date.creator = single_user
+            all_dates.append(single_date)
+        return all_dates
+
+    @classmethod
+    def sort_by_provider_name(cls):
+        query = """
+        SELECT * FROM dates 
+        LEFT JOIN users ON dates.users_id = users.id
+        ORDER BY provider_name ASC
+        ;"""
+        results = connectToMySQL(cls.db).query_db(query)
+        print(results)
+        all_dates = []
+        for one_date in results:
+            date_data = {
+                "id": one_date['id'],
+                "care_date": one_date['care_date'],
+                "provider_name": one_date['provider_name'],
+                "created_at": one_date['created_at'],
+                "updated_at": one_date['updated_at']
+            }
+            single_date = cls(date_data)
+            user_data = {
+                "id": one_date['users.id'],
+                "first_name": one_date['first_name'],
+                "last_name": one_date['last_name'],
+                "email": one_date['email'],
+                "insurance_name": one_date['insurance_name'],
+                "password": one_date['password'],
+                "created_at": one_date['users.created_at'],
+                "updated_at": one_date['users.updated_at']
+            }
+            single_user = user.User(user_data)
+            single_date.creator = single_user
+            all_dates.append(single_date)
+        return all_dates
